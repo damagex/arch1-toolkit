04b1c148c4a8b081a1687bd806a5cf788e898e49
damagex <davem1991@gmail.com>
2023-01-29 00:17:55 +0100

    .

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -2987,16 +2987,16 @@ class MaterialReader {
         this.pos = { x: pos[0].x - 8, y: pos[0].y - 12, width: 120, height: 35 };
         return this.pos;
     }
-    read(img) {
-        if (!this.pos) {
+    read(pos, img) {
+        if (!pos) {
             return null;
         }
         let buf;
         if (!img) {
-            buf = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.capture(this.pos);
+            buf = _alt1_base__WEBPACK_IMPORTED_MODULE_0__.capture(pos);
         }
         else {
-            buf = img.toData(this.pos.x, this.pos.y, this.pos.width, this.pos.height);
+            buf = img.toData(pos.x - 10, pos.y - 10, pos.width, pos.height);
         }
         let timestr = _alt1_ocr__WEBPACK_IMPORTED_MODULE_1__.findReadLine(buf, font, [[255, 255, 255], [200, 0, 0], [200, 20, 0], [210, 40, 0], [230, 80, 0], [250, 100, 0], [220, 60, 0]], 56, 23, 30, 1);
         let m = timestr.text.match(/^(\d{2}):(\d{2})$/);
@@ -3014,8 +3014,9 @@ let i = 0;
 const mr = new MaterialReader();
 const tick = setInterval(_ => {
     i++;
-    let found = mr.find(imgs.samitesilk);
-    output.innerHTML = `<div>Silke ${found ? 'Found' : 'Not Found'}! ${i}</div>`;
+    let pos = mr.find(imgs.samitesilk);
+    let count = mr.read(pos);
+    output.innerHTML = `<div>Silke ${pos ? count : 'Not Found'}! ${i}</div>`;
 }, 500);
 
 })();
--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,11 @@ export default class MaterialReader {
         this.pos = { x: pos[0].x - 8, y: pos[0].y - 12, width: 120, height: 35 };
         return this.pos;
     }
-    read(img?: ImgRef) {
-        if (!this.pos) { return null; }
+    read(pos: a1lib.RectLike, img?: ImgRef) {
+        if (!pos) { return null; }
         let buf: ImageData;
-        if (!img) { buf = a1lib.capture(this.pos); }
-        else { buf = img.toData(this.pos.x, this.pos.y, this.pos.width, this.pos.height); }
+        if (!img) { buf = a1lib.capture(pos); }
+        else { buf = img.toData(pos.x - 10, pos.y - 10, pos.width, pos.height); }
 
         let timestr = OCR.findReadLine(buf, font, [[255, 255, 255], [200, 0, 0], [200, 20, 0], [210, 40, 0], [230, 80, 0], [250, 100, 0], [220, 60, 0]], 56, 23, 30, 1);
         let m = timestr.text.match(/^(\d{2}):(\d{2})$/);
@@ -48,6 +48,7 @@ let i = 0;
 const mr = new MaterialReader();
 const tick = setInterval(_ => {
     i++;
-    let found = mr.find(imgs.samitesilk);
-    output.innerHTML = `<div>Silke ${found ? 'Found' : 'Not Found'}! ${i}</div>`;
+    let pos = mr.find(imgs.samitesilk);
+    let count = mr.read(pos);
+    output.innerHTML = `<div>Silke ${pos ? count : 'Not Found'}! ${i}</div>`;
 }, 500);
